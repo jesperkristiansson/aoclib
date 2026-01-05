c45113abf8b5881d99ea6e5681e7890e0bec300d
Jesper Kristiansson <jesper.g.kristiansson@gmail.com>
2026-01-05 13:15:47 +0100

    Add higher dimensions for Direction

--- src/Direction.hpp
+++ src/Direction.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <array>
 
+// This should be replaced with Direction2D, but that requires refactoring e.g. dir.dx to dir.dx()
 struct Direction{
     Direction() = default;
     Direction(int dx, int dy) : dx{dx}, dy{dy} {}
@@ -34,3 +35,94 @@ const inline Direction NORTH(0, -1);
 const inline Direction SOUTH(0, 1);
 const inline Direction EAST(1, 0);
 const inline Direction WEST(-1, 0);
+
+template<unsigned N>
+struct DirectionND{
+    DirectionND() = default;
+    DirectionND(std::array<int, N> values) : values(values) {}
+
+    auto operator<=>(const DirectionND<N> &other) const = default;
+
+    int operator[](size_t i) const{
+        return values[i];
+    }
+    int& operator[](size_t i){
+        return values[i];
+    }
+
+    DirectionND<N> operator*(int mult) const{
+        std::array<int, N> newValues;
+        for(size_t i = 0; i < N; i++){
+            newValues[i] = values[i] * mult;
+        }
+        return DirectionND<N>(newValues);
+    }
+    DirectionND<N> operator+(DirectionND<N> other) const{
+        std::array<int, N> newValues;
+        for(size_t i = 0; i < N; i++){
+            newValues[i] = values[i] + other.values[i];
+        }
+        return DirectionND<N>(newValues);
+    }
+
+    std::array<int, N> values{};
+};
+
+template<unsigned N>
+std::ostream &operator<<(std::ostream &os, const DirectionND<N> &d){
+    os << d[0];
+    for(size_t i = 1; i < N; i++){
+       os << ',' << d[i];
+    }
+    return os;
+}
+
+struct Direction2D : private DirectionND<2>{
+    using DirectionND<2>::DirectionND;
+    Direction2D(int dx, int dy) : DirectionND<2>({dx, dy}) {}
+
+    int dx() const{
+        return values[0];
+    }
+    int& dx(){
+        return values[0];
+    }
+    int dy() const{
+        return values[1];
+    }
+    int& dy(){
+        return values[1];
+    }
+
+    Direction2D rotateRight() const{
+        return Direction2D(-dy(), dx());
+    }
+    Direction2D rotateLeft() const{
+        return Direction2D(dy(), -dx());
+    }
+};
+
+
+struct Direction3D : private DirectionND<3>{
+    using DirectionND<3>::DirectionND;
+    Direction3D(int dx, int dy, int dz) : DirectionND<3>({dx, dy, dz}) {}
+
+    int dx() const{
+        return values[0];
+    }
+    int& dx(){
+        return values[0];
+    }
+    int dy() const{
+        return values[1];
+    }
+    int& dy(){
+        return values[1];
+    }
+    int dz() const{
+        return values[2];
+    }
+    int& dz(){
+        return values[2];
+    }
+};
